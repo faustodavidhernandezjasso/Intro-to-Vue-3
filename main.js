60d900ce8a8fc1c6e49f1303f4354054ab5c704a
Fausto David Hernández Jasso <fausto.hernandez@bunsan.io>
2025-04-08 14:42:00 -0600

    Adding dummy descriptions and dummy objects inside data object.

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: "Socks",
+            description: "Socks are necessary for not getting hurt when you are walking.",
+            stinky: "Stinky el perro",
+            cocas: "Cocas el perro"
+        }
+    }
+})
