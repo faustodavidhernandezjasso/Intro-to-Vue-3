8751b950b5397df5dccb88d8c28f8ab8e884f44a
Fausto David Hernández Jasso <fausto.hernandez@bunsan.io>
2025-04-09 18:10:05 -0600

    Adding the code challenges

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -3,6 +3,10 @@ app.component('product-display', {
     premium: {
       type: Boolean,
       required: true
+    },
+    cart: {
+        type: Number,
+        required: false
     }
   },
   template: 
@@ -39,6 +43,13 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock || this.cart == 0 }" 
+          :disabled="!inStock || this.cart == 0" 
+          v-on:click="removeFromCart">
+          Remove
+        </button>
       </div>
     </div>
   </div>`,
@@ -56,10 +67,13 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit("add-to-cart", this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      removeFromCart() {
+        this.$emit("remove-from-cart", this.variants[this.selectedVariant].id)
       }
   },
   computed: {
